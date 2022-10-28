ec7e8de0f138fd3a6e61818d76e34179621c4303
Samuel Mintah <samiblaq8@gmail.com>
2022-10-28 00:25:34 -0700

    http

--- http-example.js
+++ http-example.js
@@ -1,9 +1,10 @@
-const http = require("http");
+// const http = require("https");
+const {request} = require("https");
 
 // the request function : which takes a string as a first argument and the second argument
 //  will be a call-back that accepts response-object as an argument when called. 
  
-http.request("www.google.com" ,(res)=>{
+const req = request("https://www.google.com" ,(res)=>{
     res.on("data", (chunk)=>{
         console.log(`Data chunk : ${chunk}`);
     });
@@ -11,3 +12,6 @@ http.request("www.google.com" ,(res)=>{
         console.log('No more data');
     });
 });
+
+// always need to bring the end function because it will cause the request to be sent 
+req.end();
