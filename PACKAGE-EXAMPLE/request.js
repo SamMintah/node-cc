b923d938ce690cf7591100f4dc83c0a8b7f72b68
Samuel Mintah <samiblaq8@gmail.com>
2022-10-30 18:15:15 -0700

    adding packages

---
+++ PACKAGE-EXAMPLE/request.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+axios.get('https://www.google.com')
+ .then((response)=>{
+    console.log(response);
+})
+ .catch((error)=>{
+    console.log(error);
+ })
+  .then(()=>{
+    console.log("all done")
+  })
