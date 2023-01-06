1451c096d123b093aa0ddd77eb499cd0517081a6
Samuel Mintah <samiblaq8@gmail.com>
2023-01-05 17:19:48 -0800

    created  a single script to run both sides

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express")
 const cors = require("cors")
 const planetsRouter = require("./routes/planets/planets.router")
@@ -9,10 +10,13 @@ app.use(cors({
     origin:"http://localhost:3000"
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, ".." , 'public')))
 
 //routes
 app.use(planetsRouter)
 
-
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.join(__dirname, '..' ,'public' , 'index.hmtl'))
+// })
 
 module.exports = app;
